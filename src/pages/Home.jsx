6211395cab55612a8fd7b9430cbdfaaef6abdb2e
Alexis Pathe <alexispatheguillen@gmail.com>
2024-08-25 10:47:41 -0600

    Datos cambiados

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 export const Home =()=>{
     return(
         <div className="container">
-            <h1>Este es el Home</h1>
+            <h1>Este es el Home de la pagina</h1>
         </div>
     )
 }
