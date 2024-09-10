636c9fc8f35d7c9cc1c7d14413ae1c092eaa4859
Alexis Pathe <alexispatheguillen@gmail.com>
2024-09-09 23:54:54 -0600

    Se implementa la verficacion de tarjeta guardada en la seccion de createAccount

--- src/pages/CreateAccount.jsx
+++ src/pages/CreateAccount.jsx
@@ -1,41 +1,66 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { getFirestore, collection, doc, setDoc } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
-import { app } from '../firebaseConfig';
-import { CreditCardForm } from '../components/CreditCardForm';
-import { Form, Button, Container, Card } from 'react-bootstrap';
-import { checkUserAccount } from '../auth/checkUserAccount';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  getFirestore,
+  collection,
+  doc,
+  setDoc,
+  query,
+  where,
+  getDocs,
+} from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { app } from "../firebaseConfig";
+import { CreditCardForm } from "../components/CreditCardForm";
+import { Form, Button, Container, Card } from "react-bootstrap";
+import { checkUserAccount } from "../auth/checkUserAccount";
+
 export const CreateAccount = () => {
   const db = getFirestore(app);
-const auth = getAuth(app);
+  const auth = getAuth(app);
   const [name, setName] = useState("");
-  const [accountType, setAccountType] = useState('Ahorro');
+  const [accountType, setAccountType] = useState("Ahorro");
   const [isCardSaved, setIsCardSaved] = useState(false);
   const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+  const [status, setStatus] = useState(true);
   const navigate = useNavigate();
+
   useEffect(() => {
     const user = auth.currentUser;
     if (!user) {
-      navigate('/login');
+      navigate("/login");
     } else {
       const checkUser = async (currentUser) => {
         const check = await checkUserAccount(currentUser);
-        if (check) navigate('/perfil');
+        if (check) navigate("/perfil");
       };
       checkUser(user); // Solo se ejecuta si hay un usuario
+
+      // Verificar si ya tiene una tarjeta registrada
+      const checkCard = async (userUid) => {
+        const cardsQuery = query(
+          collection(db, "cards"),
+          where("ownerId", "==", userUid)
+        );
+        const querySnapshot = await getDocs(cardsQuery);
+
+        if (!querySnapshot.empty) {
+          setIsCardSaved(true);
+          setStatus(false)
+        }
+      };
+      checkCard(user.uid);
     }
   }, [auth.currentUser, navigate]);
-  
-  
+
   useEffect(() => {
-    // Habilita el botón si la tarjeta ha sido guardada y el tipo de cuenta está seleccionado
-    if (isCardSaved && accountType) {
+    // Habilita el botón si la tarjeta ha sido guardada y el nombre está lleno
+    if (isCardSaved && name) {
       setIsButtonDisabled(false);
     } else {
       setIsButtonDisabled(true);
     }
-  }, [isCardSaved, accountType]);
+  }, [isCardSaved, name]);
 
   const createAccount = async (userUid, userEmail) => {
     try {
@@ -50,15 +75,15 @@ const auth = getAuth(app);
         ownerId: userUid,
         email: userEmail,
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
       };
 
-      const accountsCollection = collection(db, 'accounts');
+      const accountsCollection = collection(db, "accounts");
       const accountDocRef = doc(accountsCollection, accountId);
 
       await setDoc(accountDocRef, accountData);
 
-      navigate('/perfil'); // Redirige al perfil después de crear la cuenta
+      navigate("/perfil"); // Redirige al perfil después de crear la cuenta
     } catch (error) {
       console.error("Error al crear la cuenta:", error);
     }
@@ -66,7 +91,6 @@ const auth = getAuth(app);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     const user = auth.currentUser;
     if (!user) {
       console.error("No hay usuario autenticado.");
@@ -79,19 +103,23 @@ const auth = getAuth(app);
   return (
     <Container className="my-4">
       <Card className="p-4 shadow-sm">
-        <Card.Title as="h2" className="mb-4">Crear Cuenta</Card.Title>
-        <Form >
-        <Form.Group className="mb-3">
-            <Form.Label column sm={4}>Nombre de usuario</Form.Label>
-              <Form.Control
-                type="text"
-                id="name"
-                onChange={(e) => setName(e.target.value)}
-                maxLength={50}
-                required
-
-              />
+        <Card.Title as="h2" className="mb-4">
+          Crear Cuenta
+        </Card.Title>
+        <Form>
+          <Form.Group className="mb-3">
+            <Form.Label column sm={4}>
+              Nombre de usuario
+            </Form.Label>
+            <Form.Control
+              type="text"
+              id="name"
+              onChange={(e) => setName(e.target.value)}
+              maxLength={50}
+              required
+            />
           </Form.Group>
+
           <Form.Group controlId="accountType" className="mb-3">
             <Form.Label>Tipo de Cuenta</Form.Label>
             <Form.Control
@@ -103,20 +131,20 @@ const auth = getAuth(app);
               <option value="Corriente">Corriente</option>
             </Form.Control>
           </Form.Group>
-          
-          
         </Form>
-        <CreditCardForm onCardSaved={setIsCardSaved} />
+        {/* Mostrar el formulario de tarjeta solo si no está guardada */}
+        
+        {status ? <CreditCardForm onCardSaved={setIsCardSaved} /> :<p class="text-success">Tarjeta previamente registada</p>}
+
         <Button
-            variant="primary"
-            type="button"
-            disabled={isButtonDisabled}
-            className='mt-3'
-            onClick={handleSubmit}
-          >
-            Crear Cuenta
-          
-          </Button>
+          variant="primary"
+          type="button"
+          disabled={isButtonDisabled}
+          className="mt-3"
+          onClick={handleSubmit}
+        >
+          Crear Cuenta
+        </Button>
       </Card>
     </Container>
   );
