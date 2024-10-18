7fdcfb9995620ea535d6e2a9712d0208783dca86
Alexis Pathe <alexispatheguillen@gmail.com>
2024-10-18 01:12:16 -0600

    Se implementa la informacion del usuario

--- src/routers/privateRoutes.js
+++ src/routers/privateRoutes.js
@@ -7,6 +7,7 @@ import { SoporteChat } from "../components/chatComponents/SoporteChat";
 import { PaymentDetails } from '../pages/PaymentDetails';
 import ServicePaymentForm from '../pages/ServicePaymentForm';
 import { InicioUsuario } from '../pages/InicioUsuario';
+import { EditarPerfil } from '../pages/EditarPerfil';
 export const privateRoutes = [
     { path: "/perfil", element: <Profile />, protected:true },
     { path: "/transaccion/:id", element: <TransactionDetail />, protected:true },
@@ -16,4 +17,6 @@ export const privateRoutes = [
     { path: "/pagos", element: <PaymentDetails />, protected:true },
     { path: "/pago-servicio", element: <ServicePaymentForm />, protected:true },
     { path: "/inicio-usuario", element: <InicioUsuario />, protected:true },
+    { path: "/editar-perfil", element: <EditarPerfil />, protected:true },
+
 ];
