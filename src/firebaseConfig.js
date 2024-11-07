66b49162ec1c823f7ea1cf72f45f5be75d16cb93
Alexis Pathe <alexispatheguillen@gmail.com>
2024-11-06 22:17:10 -0600

    Se implementa las variables de entorno

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,18 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDhg9ot1wlOiRGkSYQ-T6pXgaHZpxpOHjU",
-  authDomain: "untopico-b888c.firebaseapp.com",
-  projectId: "untopico-b888c",
-  storageBucket: "untopico-b888c.appspot.com",
-  messagingSenderId: "950143679973",
-  appId: "1:950143679973:web:ff670f049fa787286d830c",
-  measurementId: "G-KNYD12D1NX"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
